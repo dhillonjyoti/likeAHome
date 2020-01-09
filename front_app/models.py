55f92d17c30d08539992f25d5fb6cc1471f408a0
Dell Store Una <dhillonjyoti73@gmail.com>
2020-01-09 16:23:59 +0530

    created model

--- front_app/models.py
+++ front_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class UserRole(models.Model):
+    role_id = models.AutoField(primary_key=True)
+    role_name = models.CharField(max_length=255, default="", null=True ,unique=True, blank=True)
+
+class RoleDetails(models.Model):
+    role_id = models.ForeignKey(UserRole,on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, default="" ,null=True, blank=True)
+    email = models.EmailField(max_length=255, default="", blank=True, unique=True, primary_key=True)
+    password = models.CharField(max_length=255, default="", null=True, blank=True)
+    mobile = models.BigIntegerField(default="", null=True, blank=True)
+    address = models.TextField(max_length=255, default="", null=True, blank=True)
+    gender = models.CharField(max_length=255, default="", null=True, blank=True)
+    verify_link = models.CharField(max_length=255, default="", null=True, blank=True)
+    otp = models.CharField(max_length=255, default="", null=True, blank=True)
+    otp_time = models.CharField(max_length=255, default="", null=True, blank=True)
+    is_active = models.NullBooleanField(default=0)
