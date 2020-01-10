2c87f4306072c8aeac27d485b6b223c7ea3f2322
Dell Store Una <dhillonjyoti73@gmail.com>
2020-01-10 16:02:03 +0530

    create roledetails class

--- front_app/form.py
+++ front_app/form.py
@@ -1,8 +1,14 @@
 from django import forms
-from front_app.models import UserRole
+from front_app.models import UserRole,RoleDetails
 
 
 class UserRoleForm(forms.ModelForm):
     class Meta:
         model = UserRole
         exclude = ['role_id', 'role_name']
+
+
+class RoleDetailsForm(forms.ModelForm):
+    class Meta:
+        model = RoleDetails
+        exclude = ['name','email','password','address','gender','verify_link','otp','otp_time','is_active']
